075a1266e71b7b8accb72b675b0fae33df3b183b
Lux-63 <Lux-63@yandex.ru>
2025-07-03 20:06:15 +0500

    fix

--- script01.js
+++ script01.js
@@ -20,8 +20,9 @@ document.querySelector(".js-load-complete").remove
 document.querySelector(".js-load-bar").style.stroke="#6bdba7"
 если необходимо изменить атрибут, то
 circleBar.setAttribute("r", "108")
-Формула смены анимации, что бы в случае изменения параметров было проще редактировать.
+Формула смены анимации, что бы в случае изменения параметров было проще редактировать
 
+длина окружности. Необходима что бы расичтать в дашаррей
 
 крестик
 <svg width="24" height="24" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M20 20L4 4m16 0L4 20"/></svg>
@@ -29,7 +30,7 @@ circleBar.setAttribute("r", "108")
 
 */
 let circleBar = document.querySelector(".js-load-bar");
-let wiewStatusLoad = document.querySelector(".js-load-progress");
+let wiewStatusLoad = document.querySelector(".js-load-progress"); // Replacement of readings in the center.
 let wiewPrecentLoad = document.querySelector(".js-precent-load");
 let infoPosition = document.querySelector(".info-position");
 let radiusCircle = Number(circleBar.getAttribute("r"));
@@ -47,9 +48,9 @@ let mistakeLoad = false;
 
 function checkButton() {
     colorBar = "#cb0a0a";
-    progressBarLoad = circumference * ((100 - progressLoad) / 100)
+    progressBarLoad = circumference * ((100 - progressLoad) / 100);
     wiewPrecentLoad.innerHTML = Math.round(progressLoad);
-    // circleBar.style.stroke = colorBar
+    // circleBar.style.stroke = colorBar;
     console.log(progressLoad);
 };
 
@@ -84,39 +85,39 @@ function drawColorBar(){
         circleBar.classList.add("dots-five");
         circleBar.style.stroke = "rgb(28, 233, 17)";
         colorBar = "rgb(28, 233, 17)";
-        wiewPrecentLoad.className += "dots-clear"
+        wiewPrecentLoad.className += "dots-clear";
         infoPosition += "dots-clear";
-        wiewStatusLoad.insertAdjacentHTML('afterbegin', '<svg class="js-load-complete dots-complette" y="0px" style="enable-background:new 0 0 512.003 512.003;" xml:space="preserve" x="0px" viewBox="-400 100 1212.003 512.003"><g><g><path style="fill:rgb(28, 233, 17)" d="M507.291,57.14c-5.605-4.851-14.094-4.204-18.998,1.455L174.383,424.81l-151.39-151.39 c-5.255-5.255-13.797-5.255-19.052,0c-5.255,5.255-5.255,13.797,0,19.052l161.684,161.684c2.533,2.506,5.982,3.934,9.539,3.934 c0.162,0,0.35,0,0.539,0.027c3.746-0.162,7.276-1.886,9.701-4.716L508.773,76.138C513.597,70.479,512.95,61.99,507.291,57.14z"></path></g></g></svg>')
+        wiewStatusLoad.insertAdjacentHTML('afterbegin', '<svg class="js-load-complete dots-complette" y="0px" style="enable-background:new 0 0 512.003 512.003;" xml:space="preserve" x="0px" viewBox="-400 100 1212.003 512.003"><g><g><path style="fill:rgb(28, 233, 17)" d="M507.291,57.14c-5.605-4.851-14.094-4.204-18.998,1.455L174.383,424.81l-151.39-151.39 c-5.255-5.255-13.797-5.255-19.052,0c-5.255,5.255-5.255,13.797,0,19.052l161.684,161.684c2.533,2.506,5.982,3.934,9.539,3.934 c0.162,0,0.35,0,0.539,0.027c3.746-0.162,7.276-1.886,9.701-4.716L508.773,76.138C513.597,70.479,512.95,61.99,507.291,57.14z"></path></g></g></svg>');
         
     }
     // circleBar.style.stroke = colorBar;
     // circleBar.style.stroke = "rgb(17, 42, 233)";
-    circleBar.style.strokeDashoffset = `${progressBarLoad}`
-    console.log(radiusCircle, circumference, progressLoad, progressBarLoad)
+    circleBar.style.strokeDashoffset = `${progressBarLoad}`;
+    console.log(radiusCircle, circumference, progressLoad, progressBarLoad);
 };
 
-function stepPerogress() {
-    console.log(mistakeLoad)
+function stepProgress() {
+    console.log(mistakeLoad);
     if(StartLoading == 0){
         circleBar.style.stroke = "rgb(255, 0, 0)";
     }
     StartLoading = 1;
     if (progressLoad <= 100  && mistakeLoad === false) {
         // progressLoad += 0.03;
-        progressLoad += 0.2
-        progressBarLoad = circumference * ((100 - progressLoad) / 100)
+        progressLoad += 0.1;
+        progressBarLoad = circumference * ((100 - progressLoad) / 100);
         wiewPrecentLoad.innerHTML = Math.round(progressLoad);
         drawColorBar();
         console.log(progressLoad);
     } else {
         stopAutoLoadBar();
-        console.log("loading stopped")
+        console.log("loading stopped");
     }
 };
 function startAutoLoadBar() {
     startInterval = setInterval(() => {
         // circleBar.style.stroke = "rgb(255, 0, 0)";
-    stepPerogress()
+    stepPerogress();
     }, 10);
 };
 
@@ -126,7 +127,7 @@ function stopAutoLoadBar() {
 
 function runWarning(){
     setTimeout(clearInterval(startInterval));
-    wiewPrecentLoad.innerHTML = "warning"
+    wiewPrecentLoad.innerHTML = "warning";
     mistakeLoad = true;
     // circleBar.classList.remove("dots-three");
     circleBar.classList.add("dots-warning");
@@ -137,7 +138,7 @@ function runWarning(){
 };
 function runEror(){
     setTimeout(clearInterval(startInterval));
-    wiewPrecentLoad.innerHTML = "error"
+    wiewPrecentLoad.innerHTML = "error";
     mistakeLoad = true;
     circleBar.classList.add("dots-error");
     setTimeout(() => {
